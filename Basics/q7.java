78fb882f6f44a05e97c867d26c5aa9a5cb08b3bf
Rajesh_Achyuta <rajeshachyuta1947@gmail.com>
2025-10-28 19:33:11 +0530

    boolean value

---
+++ Basics/q7.java
@@ -0,0 +1,12 @@
+//3. Create Java Application where we have one class, it contains one non-static method like Display having parameter Boolean, it has to print the value of Boolean and it is going to return nothing then invoke this method under the main method. (Google.java)
+
+  class G {
+ void Display(boolean a){
+System.out.println(a);
+}
+ public static void main(String[] args){
+ G g =new G();
+g.Display(true);
+
+}
+} 
