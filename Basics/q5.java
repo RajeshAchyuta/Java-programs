f6ef8086bc06c5d14c5514977362b553a8c4cf3f
Rajesh_Achyuta <rajeshachyuta1947@gmail.com>
2025-10-28 19:15:02 +0530

    just display metthod

---
+++ Basics/q5.java
@@ -0,0 +1,16 @@
+//1. Create JAVA Application where we have one class, it contains one non-static method like Display, Which is going to return nothing and does not have any formal parameters, this method has to print any user-friendly message on the console while we invoke this method under the main method. (CallingMethod.java)
+   
+
+ class D{
+   void Display(){
+
+System.out.println("Which is going to return nothing and does not have any formal parameters");}
+ public static void main(String[] args){
+ D d =new D();
+ d.Display();
+
+
+
+
+}
+}
