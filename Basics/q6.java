c95cae9c43efc1e2401ba35a36ceb534136af530
Rajesh_Achyuta <rajeshachyuta1947@gmail.com>
2025-10-28 19:17:46 +0530

    string dispaly

---
+++ Basics/q6.java
@@ -0,0 +1,13 @@
+//2 Create JAVA Application where we have one class, it contains one non-static method like display, it contains one parameter like String, which is going to print the value of the string, this method is going to return nothing then, invoke this method under the main method. ( S.java)
+
+class F{
+void Dispaly(String s){
+ System.out.println(s);
+}
+public static void main(String[] args){
+  F f = new F();
+  f.Dispaly("rajesh");
+
+}
+
+}
