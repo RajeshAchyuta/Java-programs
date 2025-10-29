7d9df883ba6fb14f6a57bb1b334025f6eda3b36f
Rajesh_Achyuta <rajeshachyuta1947@gmail.com>
2025-10-29 19:55:57 +0530

    all even numbers in range

---
+++ Number Programs/even.java
@@ -0,0 +1,16 @@
+//10	Write a program to print all even numbers in range 700 to 900.
+
+import java.util.Scanner;
+class Even{
+ static Scanner sc = new Scanner(System.in);
+public static void main(String [] args){
+ for(int a = 700 ; a<=900; a++){
+if(a%2==0){
+System.out.println(a);
+}
+
+
+}
+
+}
+}
