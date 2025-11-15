075181a46c78b338ca6b767a9de96cea01fa3e0c
Rajesh_Achyuta <rajeshachyuta1947@gmail.com>
2025-11-15 12:37:06 +0530

    Cq7

---
+++ Number Programs/cq7.java
@@ -0,0 +1,16 @@
+//7.	Create a java application where we need to create any two constructors and then invoke any one constructor under the main method from there it has to invoke the remaining constructor. (E.java)
+
+class Example{
+
+Example(){
+Example a1 = new Example(10);
+System.out.println("Gjkhskjchkjsbdkjrs");
+}
+Example(int a){
+System.out.println(a);
+}
+public static void main(String [] args){
+Example a = new Example();
+
+}
+}
